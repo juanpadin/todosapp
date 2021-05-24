d8347d30be04d537fa89220b57d29e8a2982b6b0
Juan Padin <juanpadin7@gmail.com>
2021-05-24 15:33:40 -0300

    todos app

---
+++ src/components/App.js
@@ -0,0 +1,51 @@
+import React,{useState, useEffect} from 'react'
+import './App.css'
+import Form from './Form'
+import TodoList from './TodoList'
+
+const App = () => {
+    const [inputText, setInputText] = useState('');
+    const [todos, setTodos] = useState([]);
+    const [status, setStatus] = useState('all');
+    const [filteredTodos, setFilteredTodos] = useState([]);
+
+
+    useEffect(() => {
+        filterHandler()
+    },[todos,status])
+
+    const filterHandler = () =>{
+        switch(status){
+            case 'completed' : 
+                setFilteredTodos(todos.filter((todo) => todo.completed === true));
+                break;
+            case 'uncompleted':
+                setFilteredTodos(todos.filter((todo) => todo.completed === false));
+                break;
+            default:
+                setFilteredTodos(todos);
+                break;
+        }
+    }
+
+    return (
+        <div className='App'>
+            <header>
+                <h1>Juan's Todo List</h1>
+            </header>
+            <Form 
+            inputText={inputText}
+            setTodos={setTodos} 
+            todos={todos} 
+            setInputText={setInputText}
+            setStatus={setStatus}
+            
+            />
+            <TodoList setTodos={setTodos} todos={todos} filteredTodos={filteredTodos}/>
+            
+        </div>
+    )
+
+};
+
+export default App;
